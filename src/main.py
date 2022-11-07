29df1ef2981ad2fc7370f4458c957907a3039c4b
Ivan Vasilev <20002mc@gmail.com>
2022-11-07 20:27:28 +0300

    Add laugh

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from random import choice, seed
+from laugh import getLaughString
 seed(1)
 
 PROFESSIONS = ['повар', 'милиционер', 'пожарник', "строитель", "бухгалтер"]
@@ -50,3 +51,4 @@ if __name__ == '__main__':
 	p1 = Cook('повар', 'повар')
 	p2 = Cook('повар', 'повар')
 	p1.ask(p2).whatIsProfession(maybe=getRandomProfession()).logCookAnswer().logMainProfession()
+	print(getLaughString(10))
